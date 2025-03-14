d881d936fdd96c75d6320130335403cd8f5d749e
Son Nguyen <son.nguyen.xg@bp.renesas.com>
2025-03-14 12:06:12 +0700

    add script build u-boot
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

---
+++ build_u-boot.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+ARM_GCC_VERSION="SDK"
+if [ "${ARM_GCC_VERSION}" == "SDK" ] ; then
+source /opt/poky/3.1.14/environment-setup-aarch64-poky-linux
+else
+## gcc 10.3 default
+TOOLCHAIN_PATH=$HOME/toolchain/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin
+export PATH=$TOOLCHAIN_PATH:$PATH
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-none-linux-gnu-
+fi
+
+WORKPWD=$(pwd)
+UBOOT_DIR="u-boot"
+
+UBOOT_GIT_URL="git@github.com:vudangRVC/u-boot-sst.git"
+UBOOT_BRANCH="load-multi-dtb"
+UBOOT_COMMIT="1753022feeff51488ddd4539a586d62ffb4993e3"
+
+getcode_u-boot()
+{
+    cd ${WORKPWD}/
+    # download u-boot
+    if [ ! -d {UBOOT_DIR} ];then
+        git clone $UBOOT_GIT_URL ${UBOOT_DIR} --jobs 16
+        git -C ${UBOOT_DIR} checkout ${UBOOT_BRANCH}
+        git -C ${UBOOT_DIR} checkout ${UBOOT_COMMIT}
+    fi
+}
+
+mk_u-boot()
+{
+    SOC_TYPE=$1
+    cd ${WORKPWD}/${UBOOT_DIR}/
+    unset CFLAGS CPPFLAGS CXXFLAGS LDFLAGS
+    make clean
+    make distclean
+    if [ "${SOC_TYPE}" == "v2l" ] ; then
+        make smarc-rzv2l_defconfig
+    elif [ "${SOC_TYPE}" == "rzpi" ] ; then
+        make rzpi_defconfig
+    else
+        make common_defconfig
+    fi
+    make -j12
+    [ $? -ne 0 ] && log_error "Failed in ${UBOOT_DIR} ..." && exit
+}
+
+function main_process(){
+    SOC_TYPE=$1
+    getcode_u-boot
+    mk_u-boot $SOC_TYPE
+}
+
+#--start--------
+# ./build_atf.sh v2l
+# ./build_atf.sh rzpi
+main_process $*
+
+exit
+#---- end ------
