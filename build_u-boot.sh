53ba1d12e210817d15c73dbac0cdea36f6fbdbf3
sonnguyenxg <son.nguyen.xg@bp.renesas.com>
2025-05-07 09:54:23 +0700

    renesas: rzv2h: Update script to build u-boot rzv2h.
    Changes include:
    	- Update script to build u-boot rzv2h.
    
    Signed-off-by: sonnguyenxg <son.nguyen.xg@bp.renesas.com>

--- build_u-boot.sh
+++ build_u-boot.sh
@@ -4,16 +4,22 @@ source ./common.sh
 
 UBOOT_GIT_URL="git@github.com:vudangRVC/u-boot-sst.git"
 UBOOT_BRANCH="rz-support-multi-boards"
+UBOOT_BRANCH_V2H="v2021.10/rzv2h"
 
 getcode_u-boot()
 {
+    BOARD=$1
     cd ${WORKPWD}/
     # download u-boot
     if [ ! -d ${UBOOT_DIR} ]; then
         git clone $UBOOT_GIT_URL ${UBOOT_DIR} --jobs 16
     fi
     cd ${WORKPWD}/${UBOOT_DIR}
-    git checkout ${UBOOT_BRANCH}
+    if [ "${BOARD}" == "v2h" ] ; then
+        git checkout ${UBOOT_BRANCH_V2H}
+    else
+        git checkout ${UBOOT_BRANCH}
+    fi
 }
 
 mk_u-boot()
@@ -23,7 +29,11 @@ mk_u-boot()
     unset CFLAGS CPPFLAGS CXXFLAGS LDFLAGS
     make clean
     make distclean
-    make rz-multi-boards_defconfig
+    if [ "${BOARD}" == "v2h" ] ; then
+        make -j12 rzv2h-evk-ver1_defconfig
+    else
+        make rz-multi-boards_defconfig
+    fi
     make -j12
     [ $? -ne 0 ] && log_error "Failed in ${UBOOT_DIR} ..." && exit
 }
@@ -31,7 +41,7 @@ mk_u-boot()
 function main_process(){
     BOARD=$1
     validate_board "${BOARD}"
-    getcode_u-boot
+    getcode_u-boot $BOARD
     mk_u-boot $BOARD
 }
 
