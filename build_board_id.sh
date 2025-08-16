f95d6727875c26a39e52b788bec5a5f033a3b3aa
sonnguyenxg <son.nguyen.xg@bp.renesas.com>
2025-08-16 10:42:39 +0700

    Update build_board_id.sh
    Signed-off-by: sonnguyenxg <son.nguyen.xg@bp.renesas.com>

--- build_board_id.sh
+++ build_board_id.sh
@@ -5,16 +5,16 @@ build_board_id()
 {
     # Set model based on the board
     BOARD=$1
-    if [ "${BOARD}" == "v2l" ] ; then
-        MODEL="RZV2L-EVK"
-    elif [ "${BOARD}" == "rzpi" ] ; then
+    if [ "${BOARD}" == "v2l-evk" ] ; then
+        MODEL="RZV2L-evk"
+    elif [ "${BOARD}" == "g2l-sbc" ] ; then
         MODEL="RZG2L-SBC"
-    elif [ "${BOARD}" == "g2l" ] ; then
-        MODEL="RZG2L-EVK"
-    elif [ "${BOARD}" == "g2l100" ] ; then
+    elif [ "${BOARD}" == "g2l-evk" ] ; then
+        MODEL="RZG2L-evk"
+    elif [ "${BOARD}" == "g2l-100" ] ; then
         MODEL="RZG2L-100"
-    elif [ "${BOARD}" == "v2h" ] ; then
-        MODEL="RZV2H-EVK1"
+    elif [ "${BOARD}" == "v2h-evk" ] ; then
+        MODEL="RZV2H-evk1"
     else
         echo "Error: Unsupported BOARD type: ${BOARD}"
         exit 1
@@ -22,6 +22,7 @@ build_board_id()
 
     # Build bin file from platform_info.json
     cd ${WORKPWD}/tools/binmake
+    rm -rf ${WORKPWD}/tools/binmake/build
     if [ ! -d build ]; then
         mkdir -p ${WORKPWD}/tools/binmake/build
     fi
@@ -40,17 +41,37 @@ build_board_id()
     cp ${MODEL}.srec ${WORKPWD}
 }
 
+validate_board_id() {
+    BOARD=$1
+    case "${BOARD}" in
+        v2h-evk|v2l-evk|g2l-sbc|g2l-evk|g2l-100)
+            return 0
+            ;;
+        *)
+            echo "BOARD is not supported"
+            echo "Please use one of: v2h-evk, g2l-sbc, v2l-evk, g2l-evk, g2l-100"
+            echo "Example: ./all_build.sh v2h-evk"
+            echo "Example: ./all_build.sh g2l-sbc"
+            echo "Example: ./all_build.sh v2l-evk"
+            echo "Example: ./all_build.sh g2l-evk"
+            echo "Example: ./all_build.sh g2l-100"
+            exit 1
+            ;;
+    esac
+}
+
 function main_process(){
     BOARD=$1
+    validate_board_id "${BOARD}"
     build_board_id $BOARD
 }
 
 #--start--------
-# ./build_board_id.sh v2h
-# ./build_board_id.sh v2l
-# ./build_board_id.sh rzpi
-# ./build_board_id.sh g2l
-# ./build_board_id.sh g2l100
+# ./build_board_id.sh v2h-evk
+# ./build_board_id.sh v2l-evk
+# ./build_board_id.sh g2l-sbc
+# ./build_board_id.sh g2l-evk
+# ./build_board_id.sh g2l-100
 main_process $*
 
 exit
